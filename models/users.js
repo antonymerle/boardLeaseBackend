d249dcb05555b41b2964e7986b790ee8019f8ed2
Antony Merle <antony.merle@gmail.com>
2023-02-27 11:19:08 +0100

    users schema

---
+++ models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+  DOB: Date,
+  address: addressSchema,
+  phone: String,
+  profilepic: String,
+  favorites: { type: mongoose.Schema.Types.ObjectId, ref: "surfs" }, // TODO FK
+});
+
+const addressSchema = mongoose.Schema({
+  number: Number,
+  addressLine: String,
+  zipCode: Number,
+  city: String,
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
