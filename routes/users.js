88fdfa7aab0efbe894366316d36f06ca285b080b
Antony Merle <antony.merle@gmail.com>
2023-03-01 10:20:29 +0100

    modularize googleAuthVerify() in lib

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
-const { OAuth2Client } = require("google-auth-library");
+const { googleAuthVerify } = require("../lib/leaseLibrary");
 
 require("../models/connection");
 const User = require("../models/users");
@@ -98,41 +98,8 @@ router.post("/gverify", async (req, res) => {
     res.json({ result: false, error: "Token is missing" });
   }
 
-  async function googleAuthVerify() {
-    const client = new OAuth2Client(process.env.CLIENT_ID);
-    const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    console.log(payload);
-
-    const userid = payload["sub"];
-    console.log(userid);
-
-    // https://developers.google.com/identity/gsi/web/reference/js-reference?hl=fr#CredentialResponse
-    /*
-     * Les champs email, email_verified et hd vous permettent de déterminer si Google héberge une adresse e-mail et fait autorité pour celle-ci.
-     * Dans les cas où Google fait autorité, l'utilisateur est connu pour être le titulaire légitime du compte.
-     * Cas dans lesquels Google fait autorité:
-     * 1. email comporte un suffixe @gmail.com : il s'agit d'un compte Gmail.
-     * 2. email_verified est défini sur "true" et que hd est défini (il s'agit d'un compte G Suite).
-     */
-    // cas 1
-    const isTokenValid = payload.email.split("@")[1] === "gmail.com";
-
-    return {
-      isTokenValid,
-      firstname: payload.given_name,
-      lastname: payload.family_name,
-      username: payload.given_name + payload.family_name,
-      email: payload.email,
-    };
-  }
-  const response = await googleAuthVerify();
-  console.log(googleAuthVerify().catch(console.error));
+  const response = await googleAuthVerify(token);
+  // console.log(googleAuthVerify().catch(console.error));
   res.json({ result: true, ...response });
 });
 
