65f993fbae76d580eeb55fa01ec18c455cd5b526
Xavier <victoire.xavier@gmail.com>
2023-03-09 11:12:39 +0100

    update appjs cors option

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 
 const cors = require("cors");
-const corsOptions ={
-    origin:'https://board-lease-frontend.vercel.app/',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
