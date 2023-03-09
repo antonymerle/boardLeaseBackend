d3e802a18defa5fd35dcac07723629cb042e899f
Xavier <victoire.xavier@gmail.com>
2023-03-09 11:13:14 +0100

    fix de cors

--- app.js
+++ app.js
@@ -16,12 +16,7 @@ const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 
 const cors = require("cors");
-const corsOptions ={
-    origin:'https://board-lease-frontend.vercel.app/',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
