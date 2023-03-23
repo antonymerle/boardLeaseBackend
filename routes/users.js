b1efa305265ec8dad98a551dff834496efa3a6f3
Antony Merle <antony.merle@gmail.com>
2023-03-23 16:41:09 +0100

    clean code

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const uid2 = require("uid2");
 const bcrypt = require("bcrypt");
 const { googleAuthVerify } = require("../lib/leaseLibrary");
 const jwt = require("jsonwebtoken");
@@ -48,16 +47,16 @@ router.post("/signup", async (req, res) => {
 
     const isTokenValid = dataFromGoogleToken.isTokenValid;
 
-    firstname = dataFromGoogleToken.firstname;
-    lastname = dataFromGoogleToken.lastname;
-    username = dataFromGoogleToken.username;
-    email = dataFromGoogleToken.email;
-
     if (!isTokenValid)
       return res.json({
         result: false,
         error: "google connect failed to authenticate user",
       });
+
+    firstname = dataFromGoogleToken.firstname;
+    lastname = dataFromGoogleToken.lastname;
+    username = dataFromGoogleToken.username;
+    email = dataFromGoogleToken.email;
   } else if (authMethod === "classic") {
     firstname = req.body.firstname;
     lastname = req.body.lastname;
@@ -73,20 +72,6 @@ router.post("/signup", async (req, res) => {
   // We have all the necessary data, now let's check if user has not already been registered
   User.findOne({ email }).then((data) => {
     if (data === null) {
-      // const token = uid2(32);
-      // console.log(password);
-
-      // const newUser = new User({
-      //   firstname,
-      //   lastname,
-      //   username,
-      //   email,
-      //   password:
-      //     authMethod === "classic" ? bcrypt.hashSync(password, 10) : null,
-      //   // token,
-      //   favorites: [],
-      // });
-
       // a plain object is needed for jwt.sign() method
       const newUser = {
         authMethod,
@@ -122,7 +107,7 @@ router.post("/signup", async (req, res) => {
 /**
  * @name POST: users/signin
  * @desc Route serving signin form.
- * @param {{email: String, password: String}}
+ * @param {{authMethod: String, googleCredentialResponse: {{Object} | null}, email: String | null, password: String | null}}
  * @returns {{result: Boolean, token: String | null, error: String | null}}
  */
 router.post("/signin", async (req, res) => {
@@ -200,16 +185,6 @@ router.post("/signin", async (req, res) => {
 
         const jwtToken = jwt.sign(loggedUser, secretKey, { expiresIn: "1y" });
         res.json({ jwtToken });
-        // res.json({
-        //   result: true,
-        //   authMethod, // to handle lougout method on the frontend TODO: add it on redux
-        //   token: data.token,
-        //   email,
-        //   firstname: data.firstname,
-        //   lastname: data.lastname,
-        //   username: data.username,
-        //   favorites: data.favorites,
-        // });
       } else {
         res.json({ result: false, error: "Wrong password." });
       }
@@ -227,12 +202,10 @@ router.put("/", verifyJWT, (req, res) => {
   if (!firstname && !lastname && !username && !email) {
     return res.json({ result: false, error: "Missing or empty fields." });
   }
-  // console.log(firstname, lastname, username);
 
   const user = req.user;
   console.log(user);
 
-  // TODO : send back new JWT or just updated fields ?
   User.updateOne(
     { email: req.user.email },
     { firstname, lastname, username, email }
